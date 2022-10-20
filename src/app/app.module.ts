8b67f086faeac42cb0c43c1878b8313e258b8e67
yebin <yebin5127@gmail.com>
2022-10-20 21:53:29 +0900

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, Post } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from '../user/user.module';
+import { PostModule } from 'src/board/post/post.module';
+import { SchoolModule } from '../school/school.module';
+import { config } from '../config/ormconfig';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(config), UserModule, PostModule, SchoolModule],
   controllers: [AppController],
   providers: [AppService],
 })
