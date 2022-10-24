b8796c10c8b31ba028c3a327560d922e7145a779
yebin <yebin5127@gmail.com>
2022-10-24 23:14:16 +0900

    Create topic module

---
+++ src/topic/topic.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Topic } from 'src/shared/entities/topic.entity';
+import { TopicRepository } from 'src/shared/repositories/topic.repository';
+import { TopicController } from './topic.controller';
+import { TopicService } from './topic.service';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Topic])],
+    providers: [TopicService, TopicRepository],
+    controllers: [TopicController],
+})
+export class TopicModule {}
