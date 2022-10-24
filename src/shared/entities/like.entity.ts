48cc953f437e700dd9ee2dd775428dadbfc59027
yebin <yebin5127@gmail.com>
2022-10-24 22:38:48 +0900

    Create like entity

---
+++ src/shared/entities/like.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, JoinColumn, ManyToMany, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Post } from "./post.entity";
+import { User } from "./user.entity";
+
+@Entity()
+export class Like {
+    @PrimaryColumn({ name: 'user_id' })
+    user_id: number;
+
+    @PrimaryColumn({ name: 'post_id' })
+    post_id: number;
+
+    @ManyToOne(() => User, (user) => user.like)
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+
+    @ManyToOne(() => Post, (post) => post.like)
+    @JoinColumn({ name: 'post_id' })
+    post: Post;
+}
