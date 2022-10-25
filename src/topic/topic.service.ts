b3046b73344596a29f60410d8ab074a88c5764ee
yebin <yebin5127@gmail.com>
2022-10-25 20:11:55 +0900

    Add search topic Api

--- src/topic/topic.service.ts
+++ src/topic/topic.service.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Topic } from 'src/shared/entities/topic.entity';
-import { BadRequestError } from 'src/shared/exception';
+import { Topic } from "../shared/entities/topic.entity";
+import { BadRequestError, NotFoundError } from 'src/shared/exception';
 import { TopicRepository } from 'src/shared/repositories/topic.repository';
-import { TopicDto } from './topic.dto';
+
 
 @Injectable()
 export class TopicService {
     constructor(
-            public readonly topicRepository: TopicRepository
+            private readonly topicRepository: TopicRepository
         ) {}
 
-        public async addTopic(topicDto: TopicDto) {
-            const topic = await this.topicRepository.findOneTopic(topicDto);
-
+        public async addTopic(topicWord: string) {
+            const topic = await this.topicRepository.findOneTopic(topicWord);
+            console.log(topic);
             if(!topic) {
-                return await this.topicRepository.saveTopic(topicDto);
-            } else throw new BadRequestError;
+                return await this.topicRepository.saveTopic(topicWord);
+            } else throw new BadRequestError(`해당 topic은 이미 있음`);
         }
 
-        public async findTopic(topicDto: TopicDto) {
-            return await this.topicRepository.findOneTopic(topicDto);
+        public async findTopic(topicWord: string) {
+            return await this.topicRepository.findOneTopic(topicWord);
+        }
+
+        public async searchTopic(topicWord: string) {
+            const topic =  await this.topicRepository.searchTopic(topicWord);
+            console.log(topic);
+            if(topic) {
+                return topic;
+            } else throw new NotFoundError;
         }
 }
