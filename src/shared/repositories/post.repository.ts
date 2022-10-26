81ca2ec6874a191d986e6a7ff4eb9fa1c3ca8949
yebin <yebin5127@gmail.com>
2022-10-26 20:22:13 +0900

    Create post repository newApi

---
+++ src/shared/repositories/post.repository.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { PostDto } from "src/post/dto/post.dto";
+import { Repository } from "typeorm";
+import { Post } from "../entities/post.entity";
+import { User } from "../entities/user.entity";
+
+@Injectable()
+export class PostRepository {
+    constructor(
+        @InjectRepository(Post)
+        private readonly postRepository: Repository<Post>
+    ) {}
+
+    async savePost(postDto: PostDto, user: User) {
+        const post = new Post();
+
+        console.log(postDto, user);
+        post.title = postDto.title;
+        post.content = postDto.content;
+        post.school_id = postDto.school_id;
+        post.topic_id = postDto.topic_id;
+        post.image = postDto.image;
+        post.user_id = user.id;
+        
+        const newPost = await this.postRepository.save(post);
+        
+        return newPost;
+    };
+
+    async getPost(post_id: number) {
+        return await this.postRepository.createQueryBuilder('post')
+            .select('post.id')
+            .addSelect('post.title')
+            .addSelect('post.content')
+            .addSelect('post.image')
+            .addSelect('post.user_id')
+            .addSelect('post.school_id')
+            .addSelect('post.topic_id')
+            .addSelect('post.createdAt')
+            .where('post.id = :post_id', { post_id })
+            .getOne();
+    }
+}
