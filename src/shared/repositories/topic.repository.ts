235cc4264f1171d0a201a897e70ea808437095d8
yebin <yebin5127@gmail.com>
2022-10-26 20:17:58 +0900

    Update topic repository querybuilder

--- src/shared/repositories/topic.repository.ts
+++ src/shared/repositories/topic.repository.ts
@@ -11,19 +11,30 @@ export class TopicRepository {
     ) {}
     
     async saveTopic(topicWord: string) {
-        const topic = new Topic();
+        // typeOrm Active Record 버전
+        // const topic = new Topic();
 
-        topic.topic_name = topicWord;
-        const newTopic = await this.topicRepository.save(topic);
+        // topic.topic_name = topicWord;
+        // const newTopic = await this.topicRepository.save(topic);
 
-        return newTopic;
+        // return newTopic;
+
+        return await this.topicRepository.createQueryBuilder()
+            .insert()
+            .into(Topic)
+            .values([
+                { topic_name: topicWord }
+            ])
+            .execute(); 
     }
     
     async findOneTopic(topicWord: string) {
+        // typeOrm Active Record 버전
         // return await this.topicRepository.findOne({ where: { name: topicWord } });
 
         return await this.topicRepository.createQueryBuilder('topic')
             .select('topic.topic_name')
+            .addSelect('topic.id')
             .where('topic.topic_name = :topicWord', { topicWord })
             .getOne();
     }
