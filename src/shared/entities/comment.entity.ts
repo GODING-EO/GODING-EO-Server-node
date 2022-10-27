9b57cb4c513a132da9ec6ead606287d4f4d6593b
yebin <yebin5127@gmail.com>
2022-10-27 10:24:33 +0900

    Create comment entity
    댓글 entity 생성

---
+++ src/shared/entities/comment.entity.ts
@@ -0,0 +1,32 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToMany, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Post } from "./post.entity";
+import { User } from "./user.entity";
+
+@Entity()
+export class Comment {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 255, nullable: false })
+    comment: string;
+
+    @Column({ name: 'user_id' })
+    user_id: number;
+
+    @Column({ name: 'post_id' })
+    post_id: number;
+
+    @CreateDateColumn()
+    CreatedAt: Date;
+
+    @UpdateDateColumn()
+    UpdatedAt: Date;
+
+    @ManyToOne(() => User, (user) => user.post, { nullable: false })
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+
+    @ManyToOne(() => Post, (post) => post.comment, { nullable: false })
+    @JoinColumn({ name: 'post_id' })
+    post: Post;
+}
