b96d57aa631fead2b9e8ac4b82dfbc9069aed61d
yebin <yebin5127@gmail.com>
2022-10-31 16:59:44 +0900

    Fix postLike entity

--- src/shared/entities/postLike.entity.ts
+++ src/shared/entities/postLike.entity.ts
@@ -8,18 +8,18 @@ import { Post } from "./post.entity";
 import { User } from "./user.entity";
 
 @Entity()
-export class Like {
+export class PostLike {
     @PrimaryColumn({ name: 'user_id' })
     user_id: number;
 
     @PrimaryColumn({ name: 'post_id' })
     post_id: number;
 
-    @ManyToOne(() => User, (user) => user.like)
+    @ManyToOne(() => User, (user) => user.postLike)
     @JoinColumn({ name: 'user_id' })
     user: User;
 
-    @ManyToOne(() => Post, (post) => post.like)
+    @ManyToOne(() => Post, (post) => post.postlike)
     @JoinColumn({ name: 'post_id' })
     post: Post;
 }
