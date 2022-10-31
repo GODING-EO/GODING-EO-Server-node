46f3fa2b352e18c367501bd35e8932b1c4dbb998
yebin <yebin5127@gmail.com>
2022-10-31 17:10:47 +0900

    Create SchoolLike entity

---
+++ src/shared/entities/schoolLike.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { School } from "./school.entity";
+import { Topic } from "./topic.entity";
+import { User } from "./user.entity";
+
+@Entity()
+export class SchoolLike {
+    @PrimaryColumn({ name: 'user_id' })
+    user_id: number;
+
+    @PrimaryColumn({ name: 'school_id' })
+    school_id: number;
+
+    @ManyToOne(() => User, (user) => user.schoolLike)
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+
+    @ManyToOne(() => School, (school) => school.schoolLike)
+    @JoinColumn({ name: 'school_id' })
+    school: School;
+}
