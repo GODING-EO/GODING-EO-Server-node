1d9b91d6bfc3489a2deb700fb3c7332a8dea067c
yebin <yebin5127@gmail.com>
2022-11-01 22:54:20 +0900

    Update topic-like repository
    분야좋아요 기능 추가

--- src/shared/repositories/topic-like.repository.ts
+++ src/shared/repositories/topic-like.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { TopicLike } from "../entities/topicLike.entity";
+import { User } from "../entities/user.entity";
 
 @Injectable()
 export class TopicLikeRepository {
@@ -9,4 +10,24 @@ export class TopicLikeRepository {
         @InjectRepository(TopicLike)
         private readonly topicLikeRepository: Repository<TopicLike>
     ) {}
+    
+    async addTopicLike(topic_id: number, user: User) {
+        const topiclike = new TopicLike();
+
+        topiclike.topic_id = topic_id;
+        topiclike.user_id = user.id;
+
+        return await this.topicLikeRepository.save(topiclike);
+    }
+
+    async checkLike(topic_id: number, user: User) {
+        return this.topicLikeRepository.createQueryBuilder('topic_like')
+            .select('topic_like.topic_id')
+            .addSelect('topic_like.user_id')
+            .where('topic_like.topic.id = :topic_id OR topic_like.user_id = :user_id', {
+                topic_id,
+                user_id: user.id
+            })
+            .getOne()
+    }
 }
