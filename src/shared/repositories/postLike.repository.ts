1a5b869dd2eaf0c23f7dd0e65bb4566c44c170c6
yebin <yebin5127@gmail.com>
2022-11-01 19:18:39 +0900

    Update postLike repository
    게시글 좋아요 기능 추가

--- src/shared/repositories/postLike.repository.ts
+++ src/shared/repositories/postLike.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { PostLike } from "../entities/postLike.entity";
+import { User } from "../entities/user.entity";
 
 @Injectable()
 export class PostLikeRepository {
@@ -9,4 +10,23 @@ export class PostLikeRepository {
         @InjectRepository(PostLike)
         private readonly postLikeRepository: Repository<PostLike>
     ) {}
+    
+    async addPostLike(post_id: number, user: User) {
+        const postlike = new PostLike();
+
+        postlike.post_id = post_id;
+        postlike.user_id = user.id;
+        
+        return await this.postLikeRepository.save(postlike);
+    }
+
+    async checkLike(post_id: number, user: User) {
+         return await this.postLikeRepository.createQueryBuilder('post_like')
+            .select('post_like.post_id')
+            .addSelect('post_like.user_id')
+            .where('post_like.post_id = :post_id OR post_like.user_id = :user_id', {
+                post_id,
+                user_id: user.id 
+            })
+    }
 }
