eb0f865154db1a69d57264c581147ee8ae71f607
yebin <yebin5127@gmail.com>
2022-11-02 10:48:58 +0900

    Update topic-like controller
    분야 좋아요 취소 기능 url 설정

--- src/topic-like/topic-like.controller.ts
+++ src/topic-like/topic-like.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { User } from 'src/shared/entities/user.entity';
@@ -19,5 +19,18 @@ export class TopicLikeController {
             req.user as User
         );
         return { statusCode: 200, message: 'topiclike success' };
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Delete('/:topic_id/like')
+    public async cancelTopicLike(
+        @Param('topic_id') topic_id: number,
+        @Req() req: Request
+    ) {
+        await this.topicLikeService.cancelTopicLike(
+            topic_id,
+            req.user as User
+        );
+        return { statusCode: 200, message: 'Cancel topiclike success'}
     }
 }
