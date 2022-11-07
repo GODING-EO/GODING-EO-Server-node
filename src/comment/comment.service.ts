32d5dab25877d55a01200741280793e6127d695d
yebin <yebin5127@gmail.com>
2022-11-07 19:53:59 +0900

    Update comment service
    댓글 수정 기능 logic 추가

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/shared/entities/user.entity';
+import { ForbiddenError, NotFoundError } from 'src/shared/exception';
 import { CommentRepository } from 'src/shared/repositories/comment.repository';
 
 @Injectable()
@@ -10,5 +11,17 @@ export class CommentService {
 
     public async createComment(content: string, user: User, post_id: number) {
         return await this.commentRepository.createComment(content, user, post_id);
+    }
+
+    public async getOneComment(comment_id: number) {
+        return await this.commentRepository.getOneComment(comment_id);
+    }
+    
+    public async updateComment(content: string, user: User, comment_id: number) {
+        if(!await this.commentRepository.getOneComment(comment_id)) {
+            throw new NotFoundError;
+        } else if( await this.commentRepository.checkUserbyWrtier(comment_id, user) == true) {
+            return await this.commentRepository.updateComment(content, comment_id);
+        } throw new ForbiddenError;
     }
 }   
