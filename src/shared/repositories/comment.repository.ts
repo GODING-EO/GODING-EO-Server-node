9068bccbc2de3fd1085ce484393aa838d485c1a6
yebin <yebin5127@gmail.com>
2022-11-07 15:19:54 +0900

    Update comment repository
    댓글 작성, 수정 기능 추가

--- src/shared/repositories/comment.repository.ts
+++ src/shared/repositories/comment.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Comment } from "../entities/comment.entity";
+import { User } from "../entities/user.entity";
 
 @Injectable()
 export class CommentRepository{
@@ -9,4 +10,26 @@ export class CommentRepository{
         @InjectRepository(Comment)
         private readonly commentRepository: Repository<Comment>
     ) {}
+
+    async createComment(content: string, user: User, post_id: number) {
+        const comment = new Comment();
+
+        comment.content = content;
+        comment.user_id = user.id;
+        comment.post_id = post_id;
+
+        const newComment = await this.commentRepository.save(comment);
+
+        return newComment;
+    }
+
+    async updateComment(content: string, user: User, comment_id: number) {
+        return this.commentRepository.createQueryBuilder('comment')
+            .update(Comment)
+            .set({
+                content: content
+                })
+            .where('id = :comment_id', { comment_id })
+            .execute();
+    }
 }
