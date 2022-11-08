51d896c342117091d352d2c02be775f44c05980c
yebin <yebin5127@gmail.com>
2022-11-08 17:14:52 +0900

    Fix post module

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TopicLike } from 'src/shared/entities/topicLike.entity';
 import { PostRepository } from 'src/shared/repositories/post.repository';
+import { TopicLikeRepository } from 'src/shared/repositories/topic-like.repository';
 import { Post } from '../shared/entities/post.entity';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Post])],
-    providers: [PostService, PostRepository],
+    imports: [TypeOrmModule.forFeature([Post, TopicLike])],
+    providers: [PostService, PostRepository, TopicLikeRepository],
     controllers: [PostController]
 })
 export class PostModule {}
