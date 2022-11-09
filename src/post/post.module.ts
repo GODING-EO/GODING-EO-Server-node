6badaa265685da08915744f5085a203fb8fc30fd
yebin <yebin5127@gmail.com>
2022-11-09 14:20:30 +0900

    Update post module
    multer module 추가

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from 'src/shared/entities/comment.entity';
 import { PostLike } from 'src/shared/entities/postLike.entity';
@@ -7,12 +9,18 @@ import { CommentRepository } from 'src/shared/repositories/comment.repository';
 import { PostLikeRepository } from 'src/shared/repositories/post-like.repository';
 import { PostRepository } from 'src/shared/repositories/post.repository';
 import { TopicLikeRepository } from 'src/shared/repositories/topic-like.repository';
+import { multerOptionsFactory } from 'src/shared/repositories/upload.repository';
 import { Post } from '../shared/entities/post.entity';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Post, TopicLike, Comment, PostLike])],
+    imports: [
+        TypeOrmModule.forFeature([Post, TopicLike, Comment, PostLike]),
+        MulterModule.registerAsync({
+            useFactory: multerOptionsFactory,
+        }),
+    ],
     providers: [PostService, PostRepository, TopicLikeRepository, CommentRepository, PostLikeRepository],
     controllers: [PostController]
 })
