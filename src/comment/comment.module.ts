a6c84be24310a3670e1c949887a0bb27053ed5db
yebin <yebin5127@gmail.com>
2022-11-11 16:40:07 +0900

    Update comment module

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from 'src/shared/entities/comment.entity';
+import { Post } from 'src/shared/entities/post.entity';
 import { CommentRepository } from 'src/shared/repositories/comment.repository';
+import { PostRepository } from 'src/shared/repositories/post.repository';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment])],
-  providers: [CommentService, CommentRepository],
+  imports: [TypeOrmModule.forFeature([Comment, Post])],
+  providers: [CommentService, CommentRepository, PostRepository],
   controllers: [CommentController]
 })
 export class CommentModule {}
