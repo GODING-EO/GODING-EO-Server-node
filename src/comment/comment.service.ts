bdd954de08ec3e1efbec21923cf50969f0fc178b
yebin <yebin5127@gmail.com>
2022-11-11 16:39:56 +0900

    Update comment service
    게시글 없을 때 예외처리 추가

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { User } from 'src/shared/entities/user.entity';
 import { ForbiddenError, NotFoundError } from 'src/shared/exception';
 import { CommentRepository } from 'src/shared/repositories/comment.repository';
+import { PostRepository } from 'src/shared/repositories/post.repository';
 
 @Injectable()
 export class CommentService {
     constructor(
-        private readonly commentRepository: CommentRepository
+        private readonly commentRepository: CommentRepository,
+        private readonly postRepository: PostRepository
     ) {}
 
     public async createComment(content: string, user: User, post_id: number) {
+        if(!await this.postRepository.getOnePost(post_id)) throw new NotFoundError;
         return await this.commentRepository.createComment(content, user, post_id);
     }
 
