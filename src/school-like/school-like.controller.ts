10188b0e5515f11b82fdfdcade709f7146edde32
yebin <yebin5127@gmail.com>
2022-11-16 19:43:52 +0900

    Create school-like controller
    school-like api url 설정

---
+++ src/school-like/school-like.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Delete, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { User } from 'src/shared/entities/user.entity';
+import { SchoolLikeService } from './school-like.service';
+
+@Controller('school')
+export class SchoolLikeController {
+    constructor(private readonly schoolLikeService: SchoolLikeService) {}
+
+    @UseGuards(AuthGuard('jwt'))
+    @Post('/:school_id/like')
+    public async addSchoolLike(
+        @Param('school_id') school_id: number,
+        @Req() req: Request
+    ) {
+        await this.schoolLikeService.addSchoolLike(
+            school_id,
+            req.user as User
+        );
+        return { statusCode: 200, message: 'schoolLike success' };
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Delete('/:school_id/like')
+    public async cancelSchoolLike(
+        @Param('school_id') school_id: number,
+        @Req() req: Request
+    ) {
+        await this.schoolLikeService.cancelSchoolLike(
+            school_id,
+            req.user as User
+        );
+        return { statusCode: 200, message: 'Cancel schoolLike success' };
+    }
+
+}
