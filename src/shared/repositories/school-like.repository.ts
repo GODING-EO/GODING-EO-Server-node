6072ec152129d115a0e73624cebfc2b2a88b83d6
yebin <yebin5127@gmail.com>
2022-11-16 19:42:08 +0900

    Create school-like repository
    관심학교 api 생성

--- src/shared/repositories/school-like.repository.ts
+++ src/shared/repositories/school-like.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { SchoolLike } from "../entities/schoolLike.entity";
+import { User } from "../entities/user.entity";
 
 @Injectable()
 export class SchoolLikeRepository {
@@ -9,4 +10,42 @@ export class SchoolLikeRepository {
         @InjectRepository(SchoolLike)
         private readonly schoolLikeRepository: Repository<SchoolLike>
     ) {}
+
+    async addSchoolLike(school_id: number, user: User) {
+        const schoollike = new SchoolLike();
+
+        schoollike.school_id = school_id;
+        schoollike.user_id = user.id;
+
+        return await this.schoolLikeRepository.save(schoollike);
+    }
+    
+    async cancelSchoolLike(school_id: number, user: User) {
+        return await this.schoolLikeRepository.createQueryBuilder('school_like')
+            .delete()
+            .from (SchoolLike)
+            .where('school_id = :school_id AND user_id = :user_id', {
+                school_id,
+                user_id: user.id
+            })
+            .execute()
+    }
+
+    async checkLike(school_id: number, user: User) {
+        return this.schoolLikeRepository.createQueryBuilder('school_like')
+            .select('school_like.school_id')
+            .addSelect('school_like.user_id')
+            .where('school_like.school.id = :school_id AND school_like.user_id = :user_id', {
+                school_id,
+                user_id: user.id
+            })
+            .getOne()
+    }
+
+    async getLikeTopic(user: User) {
+        return this.schoolLikeRepository.createQueryBuilder('school_like')
+            .select('school_like.school_id')
+            .where('school_like.user_id = :user_id', { user_id : user.id })
+            .getMany()
+    }
 }
