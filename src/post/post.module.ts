6984980db601f31e350d8b9dc467b3a589ddff1a
yebin <yebin5127@gmail.com>
2022-11-16 23:11:08 +0900

    Update post module
    관심학교 module 추가

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from 'src/shared/entities/comment.entity';
 import { PostLike } from 'src/shared/entities/postLike.entity';
+import { SchoolLike } from 'src/shared/entities/schoolLike.entity';
 import { TopicLike } from 'src/shared/entities/topicLike.entity';
 import { CommentRepository } from 'src/shared/repositories/comment.repository';
 import { PostLikeRepository } from 'src/shared/repositories/post-like.repository';
 import { PostRepository } from 'src/shared/repositories/post.repository';
+import { SchoolLikeRepository } from 'src/shared/repositories/school-like.repository';
 import { TopicLikeRepository } from 'src/shared/repositories/topic-like.repository';
 import { multerOptionsFactory } from 'src/shared/repositories/upload.repository';
 import { Post } from '../shared/entities/post.entity';
@@ -16,12 +17,12 @@ import { PostService } from './post.service';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Post, TopicLike, Comment, PostLike]),
+        TypeOrmModule.forFeature([Post, TopicLike, SchoolLike, Comment, PostLike]),
         MulterModule.registerAsync({
             useFactory: multerOptionsFactory,
         }),
     ],
-    providers: [PostService, PostRepository, TopicLikeRepository, CommentRepository, PostLikeRepository],
+    providers: [PostService, PostRepository, TopicLikeRepository, CommentRepository, PostLikeRepository, SchoolLikeRepository],
     controllers: [PostController]
 })
 export class PostModule {}
