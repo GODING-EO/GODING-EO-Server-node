f21e5cd2d41d82a2b28c0cd260abeb48e48976ef
yebin <yebin5127@gmail.com>
2022-11-17 17:40:09 +0900

    Update user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
-import { User } from './user.entity';
+import { User } from '../shared/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
