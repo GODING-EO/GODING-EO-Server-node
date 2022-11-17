2e2087f64e9cd78cbd843885c1e3a9fe0f5629d2
yebin <yebin5127@gmail.com>
2022-11-17 09:48:18 +0900

    Update school module

--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { School } from './school.entity';
+import { School } from '../shared/entities/school.entity';
 
 @Module({
     imports: [TypeOrmModule.forFeature([School])]
