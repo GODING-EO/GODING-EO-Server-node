7ad3324ec250583a3423e5fb6f87ead88a9d6ab0
yebin <yebin5127@gmail.com>
2022-11-18 22:09:11 +0900

    Update main
    cors 설정

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(morgan("dev"));
   await app.listen(PORT);
   console.log(PORT + "번 포트에서 대기 중");
